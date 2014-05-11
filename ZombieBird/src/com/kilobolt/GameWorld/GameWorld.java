a2eafaa5d8c85cf8106e602b991aef5eff3a0875
Jon Ho <housuilun@yahoo.com>
2014-05-11 01:33:17 -0700

    day 7 complete

--- ZombieBird/src/com/kilobolt/GameWorld/GameWorld.java
+++ ZombieBird/src/com/kilobolt/GameWorld/GameWorld.java
@@ -1,21 +1,30 @@
 package com.kilobolt.GameWorld;
 
 import com.kilobolt.GameObjects.Bird;
+import com.kilobolt.GameObjects.ScrollHandler;
 
 public class GameWorld {
 
-	private Bird bird;
+    private Bird bird;
+    private ScrollHandler scroller;
 
-	public GameWorld(int midPointY) {
-		bird = new Bird(33, midPointY - 5, 17, 12);
-	}
+    public GameWorld(int midPointY) {
+        bird = new Bird(33, midPointY - 5, 17, 12);
+        // The grass should start 66 pixels below the midPointY
+        scroller = new ScrollHandler(midPointY + 66);
+    }
 
-	public void update(float delta) {
-		bird.update(delta);
-	}
+    public void update(float delta) {
+        bird.update(delta);
+        scroller.update(delta);
+    }
 
-	public Bird getBird() {
-		return bird;
+    public Bird getBird() {
+        return bird;
 
-	}
+    }
+
+    public ScrollHandler getScroller() {
+        return scroller;
+    }
 }
